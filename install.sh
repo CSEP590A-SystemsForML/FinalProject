5a9fb88e9daba8040a24e60ac79727512f161ce6
gmichailov <gmichailov@novacoast.com>
2026-05-31 00:19:23 -0700

    Fixed install script for mac.

--- install.sh
+++ install.sh
@@ -2,15 +2,17 @@
 
 set -e
 
+PYTHON=${PYTHON:-python}
+
 # Verify Python 3.12
 if ! $PYTHON -c "import sys; assert sys.version_info[:2] == (3, 12)"; then
     echo "Error: Python 3.12 is required."
     exit 1
 fi
 
-sudo apt-get update
-python3.12 -m pip install --user --upgrade pip setuptools wheel
-python3.12 -m pip install --user -r requirements/base.txt
+#sudo apt-get update
+python3.12 -m pip install --upgrade pip setuptools wheel
+python3.12 -m pip install -r requirements/base.txt
 case "$1" in
     mac)
         python3.12 -m pip install -r requirements/mac.txt
